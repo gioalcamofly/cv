998ce8d52cffd29d5a6f6754286a63f9e5f1e077
Giovanni <giovanni.alberto@hotmail.com>
2022-06-11 12:24:11 +0100

    added nav and main banner

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export function Header() {
+
+
+    return (
+        <header>
+            <nav className='navbar'>
+                <ul className='navlist'>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Portfolio</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
